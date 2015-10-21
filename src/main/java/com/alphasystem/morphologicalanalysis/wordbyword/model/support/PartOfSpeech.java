028f1a06aef39c4f2689d5571c1a1fb0f8afd0b0
sali <sali2@loyalty.com>
2015-10-20 21:15:28 -0400

    Reshuffling

--- src/main/java/com/alphasystem/morphologicalanalysis/wordbyword/model/support/PartOfSpeech.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/wordbyword/model/support/PartOfSpeech.java
@@ -22,13 +22,15 @@ public enum PartOfSpeech implements ArabicSupportEnum {
 	NOUN("Noun", "#000080", getWord(ALIF_HAMZA_BELOW, SEEN, MEEM),
 			NounProperties.class),
 
+    PROPER_NOUN("Proper Noun", "#000080", getWord(ALIF_HAMZA_BELOW, SEEN, MEEM,
+            SPACE, AIN, LAM, MEEM), NounProperties.class),
+
 	RELATIVE_PRONOUN("Relative pronoun", null, getWord(ALIF_HAMZA_BELOW, SEEN,
 			MEEM, SPACE, MEEM, WAW, SAD, WAW, LAM), NounProperties.class),
 
-	// ADJECTIVE("ADJ", getWord(SAD, FA, TA_MARBUTA)),
-
-	PROPER_NOUN("Proper Noun", "#000080", getWord(ALIF_HAMZA_BELOW, SEEN, MEEM,
-			SPACE, AIN, LAM, MEEM), NounProperties.class),
+    DEMONSTRATIVE_PRONOUN("Demonstrative pronoun", null, getWord(
+            ALIF_HAMZA_BELOW, SEEN, MEEM, SPACE, ALIF_HAMZA_BELOW, SHEEN, ALIF,
+            RA, TA_MARBUTA), NounProperties.class),
 
 	TIME_ADVERB("Time adverb", null, getWord(DTHA, RA, FA, SPACE, ZAIN, MEEM,
 			ALIF, NOON), NounProperties.class),
@@ -36,139 +38,133 @@ public enum PartOfSpeech implements ArabicSupportEnum {
 	LOCATION_ADVERB("Location adverb", null, getWord(DTHA, RA, FA, SPACE, ZAIN,
 			MEEM, KAF, ALIF, NOON), NounProperties.class),
 
-	PRONOUN("Pronoun", "#8C001A", getWord(DDAD, MEEM, YA, RA),
-			ProNounProperties.class),
-
-	DEMONSTRATIVE_PRONOUN("Demonstrative pronoun", null, getWord(
-			ALIF_HAMZA_BELOW, SEEN, MEEM, SPACE, ALIF_HAMZA_BELOW, SHEEN, ALIF,
-			RA, TA_MARBUTA), NounProperties.class),
+    PRONOUN("Pronoun", "#8C001A", getWord(DDAD, MEEM, YA, RA),
+            ProNounProperties.class),
 
 	VERB("Verb", "#4AA02C", getWord(FA, AIN, LAM), VerbProperties.class),
 
-	// IMPERATIVE_VERBAL_NOUN("IMPN", getWord(ALIF_HAMZA_BELOW, SEEN, MEEM,
-	// SPACE,
-	// FA, AIN, LAM, SPACE, _ALIF_HAMZA_ABOVE, MEEM, RA)),
+    // Particles
+
+    GENITIVE_PARTICLE("Genitive particle", "#800000", getWord(HHA, RA, FA,
+            SPACE, JEEM, RA)),
+
+    ACCUSSATIVE_PARTICLE("Accussative particle", "#8B0807", getWord(HHA, RA, FA,
+            SPACE, NOON, SAD, BA)),
 
 	DEFINITE_ARTICLE("Definite article", getWord(LAM, ALIF, MEEM, SPACE, ALIF,
 			LAM, TA, AIN, RA, YA, FA)),
 
-	NEGATIVE_PARTICLE("Negative particle", getWord(HHA, RA, FA, SPACE, NOON,
-			FA, YA)),
+    EMPHATIC_PARTICLE("Emphatic particle", getWord(ALIF, LAM, LAM, ALIF, MEEM,
+            SPACE, LAM, ALIF, MEEM, SPACE, ALIF, LAM, TA, AIN, LAM, YA, LAM)),
 
-	GENITIVE_PARTICLE("Genitive particle", "#800000", getWord(HHA, RA, FA,
-			SPACE, JEEM, RA)),
+    IMPERATIVE_PARTICLE("Imperative particle", getWord(LAM, ALIF, MEEM, SPACE,
+            ALIF, LAM, ALIF, MEEM, RA)),
 
-	ACCUSSATIVE_PARTICLE("Accussative particle", "#8B0807", getWord(HHA, RA, FA,
-			SPACE, NOON, SAD, BA)),
+    PARTICLE_OF_PURPOSE("Particle of purpose", getWord(ALIF, LAM, LAM, ALIF,
+            MEEM, SPACE, LAM, ALIF, MEEM, SPACE, ALIF, LAM, TA, WAW, KAF, YA,
+            DAL)),
 
-	CONJUNCTION_PARTICLE_WAW("Conjunction particle", getWord(ALIF, LAM, WAW,
-			ALIF, WAW, SPACE, AIN, ALIF, TTA, FA, TA_MARBUTA)),
+    CONJUNCTION_PARTICLE_WAW("Conjunction particle", getWord(ALIF, LAM, WAW,
+            ALIF, WAW, SPACE, AIN, ALIF, TTA, FA, TA_MARBUTA)),
 
-	CONJUNCTION_PARTICLE_FA("Conjunction particle", getWord(ALIF, LAM, FA,
-			ALIF, HAMZA, SPACE, AIN, ALIF, TTA, FA, TA_MARBUTA)),
+    CONJUNCTION_PARTICLE_FA("Conjunction particle", getWord(ALIF, LAM, FA,
+            ALIF, HAMZA, SPACE, AIN, ALIF, TTA, FA, TA_MARBUTA)),
 
-	SUBJUNCTIVE_PARTICLE("Subjunctive particle", getWord(HHA, RA, FA, SPACE,
-			ALIF, LAM, NOON, ALIF, SAD, BA, TA_MARBUTA)),
+    SUBORDINATING_CONJUNCTION("Subordinating conjunction", getWord(HHA, RA, FA,
+            SPACE, MEEM, SAD, DAL, RA, YA)),
 
-	JUSSIVE_PARTICLE("Jussive particle", getWord(HHA, RA, FA, SPACE, ALIF, LAM,
-			JEEM, ALIF, ZAIN, MEEM, TA_MARBUTA)),
+    AMENDMENT_PARTICLE("Amendment particle", getWord(HHA, RA, FA, SPACE, ALIF,
+            SEEN, TA, DAL, RA, ALIF, KAF)),
 
-	RESUMPTION_PARTICLE_WAW("Resumption particle", getWord(ALIF, LAM, WAW,
-			ALIF, WAW, SPACE, ALIF_HAMZA_BELOW, SEEN, TA, YA_HAMZA_ABOVE, NOON, ALIF, FA, YA,
-			TA_MARBUTA)),
+    ANSWER_PARTICLE("Answer particle", getWord(HHA, RA, FA, SPACE, JEEM, WAW,
+            ALIF, BA)),
 
-	RESUMPTION_PARTICLE_FA("Resumption particle", getWord(ALIF, LAM, FA, ALIF, HAMZA, SPACE,
-			ALIF_HAMZA_BELOW, SEEN, TA, YA_HAMZA_ABOVE, NOON, ALIF, FA, YA,
-			TA_MARBUTA)),
+    AVERSION_PARTICLE("Aversion particle", getWord(HHA, RA, FA, SPACE, RA, DAL,
+            AIN)),
 
-	EQUALIZATION_PARTICLE("Equalization particle", getWord(HHA, RA, FA, SPACE,
-			TA, SEEN, WAW, TA_MARBUTA)),
+    PARTICLE_OF_CAUSE("Particle of cause", getWord(HHA, RA, FA, SPACE, SEEN,
+            BA, YA, TA_MARBUTA)),
 
-	CIRCUMSTANTIAL_PARTICLE("Circumstantial particle", getWord(HHA, RA, FA,
-			SPACE, HHA, ALIF, LAM)),
+    PARTICLE_OF_CERTAINTY("Particle of certainty", getWord(HHA, RA, FA, SPACE,
+            TA, HHA, QAF, YA, QAF)),
 
-	RESTRICTION_PARTICLE("Restriction particle", getWord(ALIF_HAMZA_ABOVE, DAL,
-			ALIF, TA_MARBUTA, SPACE, HHA, SAD, RA)),
+    CIRCUMSTANTIAL_PARTICLE("Circumstantial particle", getWord(HHA, RA, FA,
+            SPACE, HHA, ALIF, LAM)),
 
-	PREVENTIVE_PARTICLE("Preventive particle", getWord(HHA, RA, FA, SPACE, KAF,
-			ALIF, FA)),
+    COMITATIVE_PARTICLE("Comitative particle", getWord(WAW, ALIF, WAW, SPACE,
+            ALIF, LAM, MEEM, AIN, YA, TA_MARBUTA)),
 
-	INCEPTIVE_PARTICLE("Inceptive particle", getWord(HHA, RA, FA, SPACE, ALIF,
-			BA, TA, DAL, ALIF, HAMZA)),
+    CONDITIONAL_PARTICLE("Conditional particle", getWord(HHA, RA, FA, SPACE,
+            SHEEN, RA, TTA)),
 
-	SUPPLEMENTAL_PARTICLE("Supplemental particle", getWord(HHA, RA, FA, SPACE,
-			ZAIN, ALIF, YA_HAMZA_ABOVE, DAL)),
+    EQUALIZATION_PARTICLE("Equalization particle", getWord(HHA, RA, FA, SPACE,
+            TA, SEEN, WAW, TA_MARBUTA)),
 
-	AMENDMENT_PARTICLE("Amendment particle", getWord(HHA, RA, FA, SPACE, ALIF,
-			SEEN, TA, DAL, RA, ALIF, KAF)),
+    EXHORTATION_PARTICLE("Exhortation particle", getWord(HHA, RA, FA, SPACE,
+            TA, HHA, DDAD, YA, DDAD)),
 
-	INTERROGATIVE_PARTICLE("Interrogative particle", getWord(HHA, RA, FA,
-			SPACE, ALIF, SEEN, TA, FA, HA, ALIF, MEEM)),
+    EXPLANATION_PARTICLE("Explanation particle", getWord(HHA, RA, FA, SPACE,
+            TA, FA, SAD, YA, LAM)),
 
-	CONDITIONAL_PARTICLE("Conditional particle", getWord(HHA, RA, FA, SPACE,
-			SHEEN, RA, TTA)),
+    EXCEPTIVE_PARTICLE("Exceptive particle", getWord(ALIF, DAL, ALIF,
+            TA_MARBUTA, SPACE, ALIF, SEEN, TA, THA, NOON, ALIF, HAMZA)),
 
-	VOCATIVE_PARTICLE("Vocative particle", getWord(HHA, RA, FA, SPACE, NOON,
-			DAL, ALIF, HAMZA)),
+    FUTURE_PARTICLE("Future particle", getWord(HHA, RA, FA, SPACE, ALIF, SEEN,
+            TA, QAF, BA, ALIF, LAM)),
 
-	RESULT_PARTICLE("Result particle", getWord(HHA, RA, FA, SPACE, WAW, ALIF,
-			QAF, AIN, SPACE, FA, YA, SPACE, JEEM, WAW, ALIF, BA, SPACE, ALIF,
-			LAM, SHEEN, RA, TTA)),
+    INCEPTIVE_PARTICLE("Inceptive particle", getWord(HHA, RA, FA, SPACE, ALIF,
+            BA, TA, DAL, ALIF, HAMZA)),
 
-	EXPLANATION_PARTICLE("Explanation particle", getWord(HHA, RA, FA, SPACE,
-			TA, FA, SAD, YA, LAM)),
+    PARTICLE_OF_INTERPRETATION("Particle of interpretation", getWord(HHA, RA,
+            FA, SPACE, TA, FA, SEEN, YA, RA)),
 
-	EXCEPTIVE_PARTICLE("Exceptive particle", getWord(ALIF, DAL, ALIF,
-			TA_MARBUTA, SPACE, ALIF, SEEN, TA, THA, NOON, ALIF, HAMZA)),
+    INTERROGATIVE_PARTICLE("Interrogative particle", getWord(HHA, RA, FA,
+            SPACE, ALIF, SEEN, TA, FA, HA, ALIF, MEEM)),
 
-	FUTURE_PARTICLE("Future particle", getWord(HHA, RA, FA, SPACE, ALIF, SEEN,
-			TA, QAF, BA, ALIF, LAM)),
+    NEGATIVE_PARTICLE("Negative particle", getWord(HHA, RA, FA, SPACE, NOON,
+            FA, YA)),
 
-	ANSWER_PARTICLE("Answer particle", getWord(HHA, RA, FA, SPACE, JEEM, WAW,
-			ALIF, BA)),
+    PREVENTIVE_PARTICLE("Preventive particle", getWord(HHA, RA, FA, SPACE, KAF,
+            ALIF, FA)),
 
-	RETRACTION_PARTICLE("Retraction particle", getWord(HHA, RA, FA, SPACE,
-			ALIF, DDAD, RA, ALIF, BA)),
+    PROHIBITION_PARTICLE("Prohibition particle", getWord(HHA, RA, FA, SPACE,
+            NOON, HA, YA)),
 
-	EXHORTATION_PARTICLE("Exhortation particle", getWord(HHA, RA, FA, SPACE,
-			TA, HHA, DDAD, YA, DDAD)),
+    RESUMPTION_PARTICLE_WAW("Resumption particle", getWord(ALIF, LAM, WAW,
+            ALIF, WAW, SPACE, ALIF_HAMZA_BELOW, SEEN, TA, YA_HAMZA_ABOVE, NOON, ALIF, FA, YA,
+            TA_MARBUTA)),
 
-	IMPERATIVE_PARTICLE("Imperative particle", getWord(LAM, ALIF, MEEM, SPACE,
-			ALIF, LAM, ALIF, MEEM, RA)),
+    RESUMPTION_PARTICLE_FA("Resumption particle", getWord(ALIF, LAM, FA, ALIF, HAMZA, SPACE,
+            ALIF_HAMZA_BELOW, SEEN, TA, YA_HAMZA_ABOVE, NOON, ALIF, FA, YA,
+            TA_MARBUTA)),
 
-	COMITATIVE_PARTICLE("Comitative particle", getWord(WAW, ALIF, WAW, SPACE,
-			ALIF, LAM, MEEM, AIN, YA, TA_MARBUTA)),
+    RESTRICTION_PARTICLE("Restriction particle", getWord(ALIF_HAMZA_ABOVE, DAL,
+            ALIF, TA_MARBUTA, SPACE, HHA, SAD, RA)),
 
-	SURPRISE_PARTICLE("Surprise particle", getWord(HHA, RA, FA, SPACE, FA,
-			JEEM, ALIF, HAMZA, TA_MARBUTA)),
+    RETRACTION_PARTICLE("Retraction particle", getWord(HHA, RA, FA, SPACE,
+            ALIF, DDAD, RA, ALIF, BA)),
 
-	AVERSION_PARTICLE("Aversion particle", getWord(HHA, RA, FA, SPACE, RA, DAL,
-			AIN)),
+    RESULT_PARTICLE("Result particle", getWord(HHA, RA, FA, SPACE, WAW, ALIF,
+            QAF, AIN, SPACE, FA, YA, SPACE, JEEM, WAW, ALIF, BA, SPACE, ALIF,
+            LAM, SHEEN, RA, TTA)),
 
-	PROHIBITION_PARTICLE("Prohibition particle", getWord(HHA, RA, FA, SPACE,
-			NOON, HA, YA)),
+    SUPPLEMENTAL_PARTICLE("Supplemental particle", getWord(HHA, RA, FA, SPACE,
+            ZAIN, ALIF, YA_HAMZA_ABOVE, DAL)),
 
-	EMPHATIC_PARTICLE("Emphatic particle", getWord(ALIF, LAM, LAM, ALIF, MEEM,
-			SPACE, LAM, ALIF, MEEM, SPACE, ALIF, LAM, TA, AIN, LAM, YA, LAM)),
+    SURPRISE_PARTICLE("Surprise particle", getWord(HHA, RA, FA, SPACE, FA,
+            JEEM, ALIF, HAMZA, TA_MARBUTA)),
 
-	PARTICLE_OF_PURPOSE("Particle of purpose", getWord(ALIF, LAM, LAM, ALIF,
-			MEEM, SPACE, LAM, ALIF, MEEM, SPACE, ALIF, LAM, TA, WAW, KAF, YA,
-			DAL)),
+    VOCATIVE_PARTICLE("Vocative particle", getWord(HHA, RA, FA, SPACE, NOON,
+            DAL, ALIF, HAMZA)),
 
-	PARTICLE_OF_CAUSE("Particle of cause", getWord(HHA, RA, FA, SPACE, SEEN,
-			BA, YA, TA_MARBUTA)),
+    SUBJUNCTIVE_PARTICLE("Subjunctive particle", getWord(HHA, RA, FA, SPACE,
+            ALIF, LAM, NOON, ALIF, SAD, BA, TA_MARBUTA)),
 
-	PARTICLE_OF_CERTAINTY("Particle of certainty", getWord(HHA, RA, FA, SPACE,
-			TA, HHA, QAF, YA, QAF)),
-
-	PARTICLE_OF_INTERPRETATION("Particle of interpretation", getWord(HHA, RA,
-			FA, SPACE, TA, FA, SEEN, YA, RA)),
+    JUSSIVE_PARTICLE("Jussive particle", getWord(HHA, RA, FA, SPACE, ALIF, LAM,
+            JEEM, ALIF, ZAIN, MEEM, TA_MARBUTA)),
 
 	QURANIC_INITIAL("Quranic initial", getWord(HHA, RA, FA, SPACE, MEEM, QAF,
 			TTA, AIN, TTA, TA_MARBUTA)),
-
-	SUBORDINATING_CONJUNCTION("Subordinating conjunction", getWord(HHA, RA, FA,
-			SPACE, MEEM, SAD, DAL, RA, YA)),
 
 	QURANIC_PUNCTUATION("Quranic Punctuation", getWord(QAF))
 
