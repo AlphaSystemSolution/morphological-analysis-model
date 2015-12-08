89578c45f206c005b820c727f143e08550db590e
sali <sali2@loyalty.com>
2015-12-08 16:48:22 -0500

    convenience method to print root (used to search dictionary)

--- src/main/java/com/alphasystem/morphologicalanalysis/wordbyword/model/support/RootWord.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/wordbyword/model/support/RootWord.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.alphasystem.morphologicalanalysis.wordbyword.model.support;
 
@@ -12,102 +12,111 @@ import static org.apache.commons.lang3.StringUtils.isBlank;
 
 /**
  * @author sali
- * 
  */
 public class RootWord extends AbstractDocument {
 
-	private static final long serialVersionUID = 1870389910904806332L;
+    private static final long serialVersionUID = 1870389910904806332L;
 
-	protected ArabicLetterType firstRadical;
+    protected ArabicLetterType firstRadical;
 
-	protected ArabicLetterType secondRadical;
+    protected ArabicLetterType secondRadical;
 
-	protected ArabicLetterType thirdRadical;
+    protected ArabicLetterType thirdRadical;
 
-	protected ArabicLetterType fourthRadical;
+    protected ArabicLetterType fourthRadical;
 
-	public RootWord() {
-		this(null, null, null, null);
-	}
+    public RootWord() {
+        this(null, null, null, null);
+    }
 
-	@PersistenceConstructor
-	public RootWord(ArabicLetterType firstRadical,
-			ArabicLetterType secondRadical, ArabicLetterType thirdRadical,
-			ArabicLetterType fourthRadical) {
-		super(nextId());
-		this.firstRadical = firstRadical;
-		this.secondRadical = secondRadical;
-		this.thirdRadical = thirdRadical;
-		this.fourthRadical = fourthRadical;
-		updateDisplayName();
-	}
+    @PersistenceConstructor
+    public RootWord(ArabicLetterType firstRadical,
+                    ArabicLetterType secondRadical, ArabicLetterType thirdRadical,
+                    ArabicLetterType fourthRadical) {
+        super(nextId());
+        this.firstRadical = firstRadical;
+        this.secondRadical = secondRadical;
+        this.thirdRadical = thirdRadical;
+        this.fourthRadical = fourthRadical;
+        updateDisplayName();
+    }
 
-	/**
-	 * Copy Constructor.
-	 *
-	 * @param src
-	 * @throws NullPointerException
-	 */
-	public RootWord(RootWord src) throws NullPointerException{
-		super(nextId());
-		if(src == null){
-			throw new NullPointerException("Source is null.");
-		}
-		setFirstRadical(src.getFirstRadical());
-		setSecondRadical(src.getSecondRadical());
-		setThirdRadical(src.getThirdRadical());
-		setFourthRadical(src.getFourthRadical());
-		updateDisplayName();
-	}
+    /**
+     * Copy Constructor.
+     *
+     * @param src
+     * @throws NullPointerException
+     */
+    public RootWord(RootWord src) throws NullPointerException {
+        super(nextId());
+        if (src == null) {
+            throw new NullPointerException("Source is null.");
+        }
+        setFirstRadical(src.getFirstRadical());
+        setSecondRadical(src.getSecondRadical());
+        setThirdRadical(src.getThirdRadical());
+        setFourthRadical(src.getFourthRadical());
+        updateDisplayName();
+    }
 
-	public ArabicLetterType getFirstRadical() {
-		return firstRadical;
-	}
+    public ArabicLetterType getFirstRadical() {
+        return firstRadical;
+    }
 
-	public void setFirstRadical(ArabicLetterType firstRadical) {
-		this.firstRadical = firstRadical;
-	}
+    public void setFirstRadical(ArabicLetterType firstRadical) {
+        this.firstRadical = firstRadical;
+    }
 
-	public ArabicLetterType getFourthRadical() {
-		return fourthRadical;
-	}
+    public ArabicLetterType getFourthRadical() {
+        return fourthRadical;
+    }
 
-	public void setFourthRadical(ArabicLetterType fourthRadical) {
-		this.fourthRadical = fourthRadical;
-	}
+    public void setFourthRadical(ArabicLetterType fourthRadical) {
+        this.fourthRadical = fourthRadical;
+    }
 
-	public ArabicLetterType getSecondRadical() {
-		return secondRadical;
-	}
+    public ArabicLetterType getSecondRadical() {
+        return secondRadical;
+    }
 
-	public void setSecondRadical(ArabicLetterType secondRadical) {
-		this.secondRadical = secondRadical;
-	}
+    public void setSecondRadical(ArabicLetterType secondRadical) {
+        this.secondRadical = secondRadical;
+    }
 
-	public ArabicLetterType getThirdRadical() {
-		return thirdRadical;
-	}
+    public ArabicLetterType getThirdRadical() {
+        return thirdRadical;
+    }
 
-	public void setThirdRadical(ArabicLetterType thirdRadical) {
-		this.thirdRadical = thirdRadical;
-	}
+    public void setThirdRadical(ArabicLetterType thirdRadical) {
+        this.thirdRadical = thirdRadical;
+    }
 
-	private String updateBuilder(ArabicLetterType src) {
-		StringBuilder builder = new StringBuilder();
-		String value = src == null ? "" : src.name();
-		if (!isBlank(value)) {
-			builder.append(":").append(value);
-		}
-		return builder.toString();
-	}
+    public String toCode() {
+        StringBuilder builder = new StringBuilder();
+        builder.append((firstRadical == null) ? "" : firstRadical.toCode())
+                .append((secondRadical == null) ? "" : secondRadical.toCode())
+                .append((thirdRadical == null) ? "" : thirdRadical.toCode())
+                .append((fourthRadical == null) ? "" : fourthRadical.toCode());
+        String result = builder.toString().trim();
+        return isBlank(result) ? null : result;
+    }
 
-	public void updateDisplayName() {
-		StringBuilder builder = new StringBuilder();
-		builder.append(id).append(updateBuilder(firstRadical))
-				.append(updateBuilder(secondRadical))
-				.append(updateBuilder(thirdRadical))
-				.append(updateBuilder(fourthRadical));
-		setDisplayName(builder.toString());
-	}
+    private String updateBuilder(ArabicLetterType src) {
+        StringBuilder builder = new StringBuilder();
+        String value = src == null ? "" : src.name();
+        if (!isBlank(value)) {
+            builder.append(":").append(value);
+        }
+        return builder.toString();
+    }
+
+    public void updateDisplayName() {
+        StringBuilder builder = new StringBuilder();
+        builder.append(id).append(updateBuilder(firstRadical))
+                .append(updateBuilder(secondRadical))
+                .append(updateBuilder(thirdRadical))
+                .append(updateBuilder(fourthRadical));
+        setDisplayName(builder.toString());
+    }
 
 }
