059e8d470fe70a1e319118ad42f724cd73a04079
sali <sali2@loyalty.com>
2015-12-18 08:18:40 -0500

    Added verbal nouns, noun of place and time, and cofiguartion

--- src/main/java/com/alphasystem/morphologicalanalysis/morphology/model/MorphologicalEntry.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/morphology/model/MorphologicalEntry.java
@@ -1,10 +1,15 @@
 package com.alphasystem.morphologicalanalysis.morphology.model;
 
 import com.alphasystem.arabic.model.NamedTemplate;
+import com.alphasystem.morphologicalanalysis.morphology.model.support.NounOfPlaceAndTime;
+import com.alphasystem.morphologicalanalysis.morphology.model.support.VerbalNoun;
 import com.alphasystem.persistence.model.AbstractDocument;
 import org.springframework.data.annotation.TypeAlias;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 import static java.lang.String.format;
 
@@ -19,6 +24,12 @@ public class MorphologicalEntry extends AbstractDocument {
     protected RootLetters rootLetters;
 
     protected NamedTemplate form;
+
+    protected Set<VerbalNoun> verbalNouns;
+
+    protected Set<NounOfPlaceAndTime> nounOfPlaceAndTimes;
+
+    protected ConjugationConfiguration configuration;
 
     protected String dictionary;
 
@@ -36,6 +47,42 @@ public class MorphologicalEntry extends AbstractDocument {
 
     public void setRootLetters(RootLetters rootLetters) {
         this.rootLetters = rootLetters;
+    }
+
+    public Set<VerbalNoun> getVerbalNouns() {
+        if (verbalNouns == null) {
+            verbalNouns = new LinkedHashSet<>();
+        }
+        return verbalNouns;
+    }
+
+    public void setVerbalNouns(Set<VerbalNoun> verbalNouns) {
+        this.verbalNouns = new LinkedHashSet<>();
+        if (verbalNouns != null) {
+            this.verbalNouns.addAll(verbalNouns);
+        }
+    }
+
+    public Set<NounOfPlaceAndTime> getNounOfPlaceAndTimes() {
+        if (nounOfPlaceAndTimes == null) {
+            nounOfPlaceAndTimes = new LinkedHashSet<>();
+        }
+        return nounOfPlaceAndTimes;
+    }
+
+    public void setNounOfPlaceAndTimes(Set<NounOfPlaceAndTime> nounOfPlaceAndTimes) {
+        this.nounOfPlaceAndTimes = new LinkedHashSet<>();
+        if (nounOfPlaceAndTimes != null) {
+            this.nounOfPlaceAndTimes.addAll(nounOfPlaceAndTimes);
+        }
+    }
+
+    public ConjugationConfiguration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(ConjugationConfiguration configuration) {
+        this.configuration = configuration;
     }
 
     public String getDictionary() {
