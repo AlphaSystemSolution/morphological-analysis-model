00967f6ae46c68b5c377882d1d4f9b6586fa1f79
sali <sali2@loyalty.com>
2015-12-18 22:02:00 -0500

    moving service related methods to RepositoryUtil

--- src/main/java/com/alphasystem/morphologicalanalysis/common/model/VerseTokenPairGroup.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/common/model/VerseTokenPairGroup.java
@@ -3,6 +3,9 @@ package com.alphasystem.morphologicalanalysis.common.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.util.Collections.addAll;
+import static org.apache.commons.lang3.ArrayUtils.isNotEmpty;
+
 /**
  * @author sali
  */
@@ -10,9 +13,10 @@ public class VerseTokenPairGroup {
 
     protected Integer chapterNumber;
     protected List<VerseTokensPair> pairs;
+    protected boolean includeHidden;
 
     public VerseTokenPairGroup() {
-        setPairs(null);
+        setPairs((VerseTokensPair[]) null);
     }
 
     public Integer getChapterNumber() {
@@ -23,8 +27,23 @@ public class VerseTokenPairGroup {
         this.chapterNumber = chapterNumber;
     }
 
+    public boolean isIncludeHidden() {
+        return includeHidden;
+    }
+
+    public void setIncludeHidden(boolean includeHidden) {
+        this.includeHidden = includeHidden;
+    }
+
     public List<VerseTokensPair> getPairs() {
         return pairs;
+    }
+
+    public void setPairs(VerseTokensPair... pairs) {
+        this.pairs = new ArrayList<>();
+        if (isNotEmpty(pairs)) {
+            addAll(this.pairs, pairs);
+        }
     }
 
     public void setPairs(List<VerseTokensPair> pairs) {
