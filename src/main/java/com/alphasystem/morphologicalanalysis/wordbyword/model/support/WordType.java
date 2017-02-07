9c5cf73c8241b4e9879cfbf74ed6b9e608ccec33
sfali23 <f.syed.ali@gmail.com>
2017-02-07 17:06:16 -0500

    Revert "implemnting ArabicSupportEnum"
    This reverts commit 9a9054750e87ee030286a6893dcdcb941ff06828.

--- src/main/java/com/alphasystem/morphologicalanalysis/wordbyword/model/support/WordType.java
+++ src/main/java/com/alphasystem/morphologicalanalysis/wordbyword/model/support/WordType.java
@@ -1,53 +1,18 @@
 package com.alphasystem.morphologicalanalysis.wordbyword.model.support;
 
-import com.alphasystem.arabic.model.ArabicSupportEnum;
-import com.alphasystem.arabic.model.ArabicWord;
-
-import static com.alphasystem.arabic.model.ArabicLetterType.AIN;
-import static com.alphasystem.arabic.model.ArabicLetterType.ALIF_HAMZA_BELOW;
-import static com.alphasystem.arabic.model.ArabicLetterType.DDAD;
-import static com.alphasystem.arabic.model.ArabicLetterType.FA;
-import static com.alphasystem.arabic.model.ArabicLetterType.HHA;
-import static com.alphasystem.arabic.model.ArabicLetterType.LAM;
-import static com.alphasystem.arabic.model.ArabicLetterType.MEEM;
-import static com.alphasystem.arabic.model.ArabicLetterType.RA;
-import static com.alphasystem.arabic.model.ArabicLetterType.SEEN;
-import static com.alphasystem.arabic.model.ArabicLetterType.YA;
-import static com.alphasystem.arabic.model.ArabicWord.getWord;
-
 /**
  * @author sali
  */
-public enum WordType implements ArabicSupportEnum {
+public enum WordType {
 
-    NOUN("Noun", getWord(ALIF_HAMZA_BELOW, SEEN, MEEM), NounPartOfSpeechType.values()),
-    PRO_NOUN("Pronoun", getWord(DDAD, MEEM, YA, RA), ProNounPartOfSpeechType.values()),
-    VERB("Verb", getWord(FA, AIN, LAM), VerbPartOfSpeechType.values()),
-    PARTICLE("Particle", getWord(HHA, RA, FA), ParticlePartOfSpeechType.values());
+    NOUN(NounPartOfSpeechType.values()),
+    PRO_NOUN(ProNounPartOfSpeechType.values()),
+    VERB(VerbPartOfSpeechType.values()),
+    PARTICLE(ParticlePartOfSpeechType.values());
     private final PartOfSpeechType[] partOfSpeeches;
 
-    private final String code;
-    private final ArabicWord label;
-
-    WordType(String code, ArabicWord label, PartOfSpeechType... partOfSpeeches) {
-        this.code = code;
-        this.label = label;
+    WordType(PartOfSpeechType... partOfSpeeches) {
         this.partOfSpeeches = partOfSpeeches;
-    }
-
-    @Override
-    public String getCode() {
-        return code;
-    }
-
-    @Override
-    public String getName() {
-        return name();
-    }
-
-    @Override
-    public ArabicWord toLabel() {
-        return label;
     }
 
     public PartOfSpeechType[] getPartOfSpeeches() {
