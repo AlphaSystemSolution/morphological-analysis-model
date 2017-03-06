1cba48a767fab16ba4d9176a6de19f1af248dac6
sfali23 <f.syed.ali@gmail.com>
2017-03-06 09:47:26 -0500

    adding id and implementing Comparable to compare

--- morphological-analysis-simple-model/src/main/java/com/alphasystem/morphologicalanalysis/common/model/VerseTokenPairGroup.java
+++ morphological-analysis-simple-model/src/main/java/com/alphasystem/morphologicalanalysis/common/model/VerseTokenPairGroup.java
@@ -1,19 +1,34 @@
 package com.alphasystem.morphologicalanalysis.common.model;
 
+import com.alphasystem.util.AppUtil;
+import com.alphasystem.util.IdGenerator;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * @author sali
  */
-public class VerseTokenPairGroup {
+public class VerseTokenPairGroup implements Comparable<VerseTokenPairGroup> {
 
+    private final String id;
     private Integer chapterNumber;
     private List<VerseTokensPair> pairs;
     private boolean includeHidden;
 
     public VerseTokenPairGroup() {
+        this(0);
+    }
+
+    public VerseTokenPairGroup(Integer chapterNumber) {
+        this.chapterNumber = chapterNumber;
+        this.id = IdGenerator.nextId();
         setPairs(null);
+    }
+
+    public String getId() {
+        return id;
     }
 
     public Integer getChapterNumber() {
@@ -43,12 +58,36 @@ public class VerseTokenPairGroup {
         }
     }
 
+    public void addPairs(VerseTokensPair... pairs) {
+        Collections.addAll(this.pairs, pairs);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        boolean result = super.equals(obj);
+        if(AppUtil.isInstanceOf(VerseTokenPairGroup.class, obj)){
+            VerseTokenPairGroup o = (VerseTokenPairGroup) obj;
+            result = getId().equals(o.getId());
+        }
+        return result;
+    }
+
+    @Override
+    public int compareTo(VerseTokenPairGroup o) {
+        int result = 1;
+        if(o != null){
+            result = getId().compareTo(o.getId());
+        }
+        return result;
+    }
+
     @Override
     public String toString() {
         if (pairs.isEmpty()) {
             return super.toString();
         }
         StringBuilder builder = new StringBuilder();
+        builder.append(chapterNumber).append("||");
         builder.append(pairs.get(0));
         for (int i = 1; i < pairs.size(); i++) {
             builder.append("|").append(pairs.get(i));
