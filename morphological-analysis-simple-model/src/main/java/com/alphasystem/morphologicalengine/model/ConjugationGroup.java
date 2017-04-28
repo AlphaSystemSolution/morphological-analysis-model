522f5fdf62d4cfa57d723993c5f0b171ce110571
sfali23 <f.syed.ali@gmail.com>
2017-04-28 07:03:14 -0400

    adding id for detailed conjugation

--- morphological-analysis-simple-model/src/main/java/com/alphasystem/morphologicalengine/model/ConjugationGroup.java
+++ morphological-analysis-simple-model/src/main/java/com/alphasystem/morphologicalengine/model/ConjugationGroup.java
@@ -9,12 +9,22 @@ public class ConjugationGroup {
 
     private SarfTermType termType;
 
+    private String id;
+
     public SarfTermType getTermType() {
         return termType;
     }
 
     public void setTermType(SarfTermType termType) {
         this.termType = termType;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String defaultValue() {
